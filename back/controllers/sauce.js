a627c60f9ebddb57a2feb2c3f030a1af3f52f34a
mserr113 <104605862+mserr113@users.noreply.github.com>
2023-01-01 18:07:13 -0500

    updated controllers for images

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,19 +1,21 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
+    req.body.sauce = JSON.parse(req.body.sauce);  
+    const url = req.protocol  + '://' + req.get('host');  
     const sauce = new Sauce({
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        imageUrl: req.body.imageUrl,
-        mainPepper: req.body.mainPepper,
-        //need to hard set these values to be empty arrays/zeroes
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
-        userId: req.body.userId,
+        userId: req.body.sauce.userId,
+        name: req.body.sauce.name,
+        manufacturer: req.body.sauce.manufacturer,
+        description: req.body.sauce.description,
+        mainPepper: req.body.sauce.mainPepper,
+        imageUrl: url + '/images/' + req.file.filename,
+        heat: req.body.sauce.heat,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: [],
     });
     sauce.save().then(
         () => {
@@ -47,21 +49,42 @@ exports.getOneSauce = (req, res, next) => {
   };
 
 exports.modifySauce = (req, res, next) => {
-    const sauce = new Sauce({
-      name: req.body.name,
-      manufacturer: req.body.manufacturer,
-      description: req.body.description,
-      heat: req.body.heat,
-      //need to maintain likes and dislikes
-      likes: req.body.likes,
-      dislikes: req.body.dislikes,
-      imageUrl: req.body.imageUrl,
-      mainPepper: req.body.mainPepper,
-      //need to hard set these values to be empty arrays/zeroes
-      usersLiked: req.body.usersLiked,
-      usersDisliked: req.body.usersDisliked,
-      userId: req.body.userId,
-    });
+    let sauce = new Sauce({_id: req.params._id });
+    if(req.file) {
+      req.body.sauce = JSON.parse(req.body.sauce);  
+      const url = req.protocol  + '://' + req.get('host');  
+      sauce = {
+        _id: req.params.id,
+        userId: req.body.sauce.userId,
+        name: req.body.sauce.name,
+        manufacturer: req.body.sauce.manufacturer,
+        description: req.body.sauce.description,
+        mainPepper: req.body.sauce.mainPepper,
+        imageUrl: url + '/images/' + req.file.filename,
+        heat: req.body.sauce.heat,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: [],
+    };
+    } else {
+        sauce = {
+        _id: req.params.id,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        heat: req.body.heat,
+        //need to maintain likes and dislikes
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        imageUrl: req.body.imageUrl,
+        mainPepper: req.body.mainPepper,
+        //need to hard set these values to be empty arrays/zeroes
+        usersLiked: req.body.usersLiked,
+        usersDisliked: req.body.usersDisliked,
+        userId: req.body.userId,
+      };
+    }
     Sauce.updateOne({_id: req.params.id}, sauce).then(
       () => {
         res.status(201).json({
@@ -78,20 +101,27 @@ exports.modifySauce = (req, res, next) => {
   };
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  };
+  Sauce.findOne({_id: req.params.id}).then(
+    (sauce) => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink('images/' + filename, () => {
+        Sauce.deleteOne({_id: req.params.id}).then(
+          () => {
+            res.status(200).json({
+              message: 'Deleted!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      });
+    }
+  );
+};
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find().then(
